56ee0af909ea06d1e805d85bd4970e4d2cb65102
netop://ウエハ <paul@webb.page>
2019-06-19 16:07:55 -0500

    Account for channel

--- src/platforms/web/server.js
+++ src/platforms/web/server.js
@@ -9,14 +9,27 @@ const headRegex = /(<head>).*(<\/head>)/g;
 app.use(express.static(__dirname));
 
 app.get('*', async(req, res) => {
-  if (req.url.includes('/$/')) {
-    res.sendFile(path.join(__dirname, '/index.html'));
+  let finalHtml = '';
+  let identifier = req.url.split('/').pop();
+  let isAppPage = req.url.includes('/$/');
+  let isChannel = req.url.includes('/@');
+  let queryUrl = '';
+
+  switch (true) {
+    case isAppPage:
+      res.sendFile(path.join(__dirname, '/index.html'));
+      break;
+
+    case isChannel:
+      queryUrl = `https://chainquery.lbry.io/api/sql?query=SELECT channel.name as channel, claim.name, claim.description, claim.language, claim.thumbnail_url, claim.title FROM claim left join claim channel on claim.publisher_id = channel.claim_id where claim.claim_id="${identifier}"`;
+      break;
+
+    default: // isContentPage
+      queryUrl = `https://chainquery.lbry.io/api/sql?query=SELECT * FROM claim where claim_id="${identifier}"`;
+      break;
   }
 
-  const claimId = req.url.split('/').pop();
-  const queryUrl = `https://chainquery.lbry.io/api/sql?query=SELECT * FROM claim where claim_id="${claimId}"`;
-
-  const [finalHtml] = await Promise.all([fetch(queryUrl)
+  [finalHtml] = await Promise.all([fetch(queryUrl)
     .then(response => response.json())
     .then(claim => {
       if (!claim.success || !claim.data[0]) {
@@ -28,9 +41,9 @@ app.get('*', async(req, res) => {
       // const claimAuthor = claim.channel_name ? claim.channel_name : 'Anonymous';
       const claimDescription = claim.description && claim.description.length > 0 ? claim.description : 'Visit LBRY.tv for more content!';
       const claimLanguage = claim.language || 'en_US';
-      const claimThumnail = claim.thumbnail_url || '/og.png';
+      const claimThumbnail = claim.thumbnail_url || '/og.png';
       const claimTitle = `${claim.title} on LBRY.tv` || 'LBRY.tv'; // `${claim.title} from ${claimAuthor} on LBRY.tv`;
-      const claimUrl = `https://beta.lbry.tv/${claim.name}/${claim.claim_id}` || 'https://beta.lbry.tv';
+      const claimUrl = `https://beta.lbry.tv/${claim.name}${claim.claim_id ? (`/${claim.claim_id}`) : ''}` || 'https://beta.lbry.tv';
       // This comment was inlined and preventing the rest of the script to load
       const indexHtml = readFileSync(path.join(__dirname, '/index.html'), 'utf8').replace(/\s\s/gm, '').replace(/>\s</gm, '><').replace('// Use relative path if we are in electron', '');
       let finalTags = '';
@@ -38,9 +51,9 @@ app.get('*', async(req, res) => {
       finalTags += '<meta charset="utf8"/>';
       finalTags += `<meta name="description" content="${claimDescription}"/>`;
       finalTags += `<meta name="keywords" content="${claim.tags ? claim.tags.toString() : ''}"/>`;
-      finalTags += `<meta name="twitter:image" content="${claimThumnail}"/>`;
+      finalTags += `<meta name="twitter:image" content="${claimThumbnail}"/>`;
       finalTags += `<meta property="og:description" content="${claimDescription}"/>`;
-      finalTags += `<meta property="og:image" content="${claimThumnail}"/>`;
+      finalTags += `<meta property="og:image" content="${claimThumbnail}"/>`;
       finalTags += `<meta property="og:locale" content="${claimLanguage}"/>`;
       finalTags += `<meta property="og:site_name" content="LBRY.tv"/>`;
       finalTags += `<meta property="og:type" content="website"/>`;
